0f020373d3bf0c23763d59f450aae31dd5215930
aishaansari29 <44176334+aishaansari29@users.noreply.github.com>
2018-10-19 14:39:53 +0530

    Create checkvowel.py

---
+++ checkvowel.py
@@ -0,0 +1,18 @@
+ch=input()
+if ( (ch >= 'a' and ch <= 'z') or 
+             (ch >= 'A' and ch <= 'Z') ):  
+  
+            # To handle upper case letters 
+            ch = ch.lower() 
+  
+            if (ch == 'a' or ch == 'e' or ch == 'i' 
+                        or ch == 'o' or ch == 'u'):
+                    print("Vowel")
+                
+            else:
+                    print("Consonant")
+
+
+else:
+        print("Invalid")
+
