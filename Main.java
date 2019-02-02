7d78d3bfb98110e88df63c9ad6eced50c1b4ceb2
aishaansari29 <44176334+aishaansari29@users.noreply.github.com>
2019-02-03 00:05:14 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.math.*;
+
+
+ public class Main
+{
+  public static void main(String args[]){
+
+int n,k;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+    
+k=sc.nextInt();
+int p=(int)Math.pow(n,k);
+System.out.println(p);
+}
+}
+
+
+
