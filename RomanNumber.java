5a09f964c5bc9555f66ab7c856f1b7174cf9025a
aishaansari29 <44176334+aishaansari29@users.noreply.github.com>
2019-03-10 00:03:33 +0530

    Create RomanNumber.java

---
+++ RomanNumber.java
@@ -0,0 +1,71 @@
+import java.util.*; 
+  
+class RomanNumber 
+{ 
+    // This function returns value of a Roman symbol 
+    int value(char r) 
+    { 
+        if (r == 'I') 
+            return 1; 
+        if (r == 'V') 
+            return 5; 
+        if (r == 'X') 
+            return 10; 
+        
+        return -1; 
+    } 
+  
+    // Finds decimal value of a given romal numeral 
+    int romanToDecimal(String str) 
+    { 
+        // Initialize result 
+        int res = 0; 
+  
+        for (int i=0; i<str.length(); i++) 
+        { 
+            // Getting value of symbol s[i] 
+            int s1 = value(str.charAt(i)); 
+  
+            // Getting value of symbol s[i+1] 
+            if (i+1 <str.length()) 
+            { 
+                int s2 = value(str.charAt(i+1)); 
+  
+                // Comparing both values 
+                if (s1 >= s2) 
+                { 
+                    // Value of current symbol is greater 
+                    // or equalto the next symbol 
+                    res = res + s1; 
+                } 
+                else
+                { 
+                    res = res + s2 - s1; 
+                    i++; // Value of current symbol is 
+                    // less than the next symbol 
+                } 
+            } 
+            else
+            { 
+                res = res + s1; 
+                i++; 
+            } 
+        } 
+  
+        return res; 
+    } 
+  
+    // Driver method 
+    public static void main(String args[]) 
+    { 
+        RomanNumber ob = new RomanNumber(); 
+  
+        // Considering inputs given are valid 
+       Scanner scan= new Scanner(System.in);
+
+
+
+String str=scan.nextLine();
+        System.out.println(ob.romanToDecimal(str)); 
+    } 
+} 
