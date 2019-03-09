7f3ce1f9ff83079a63ae0573d14c3877c3bb56b2
aishaansari29 <44176334+aishaansari29@users.noreply.github.com>
2019-03-10 00:28:25 +0530

    Create holiday.c

---
+++ holiday.c
@@ -0,0 +1,26 @@
+int main() {  
+    int day;
+   
+    
+    scanf("%d", &day);  
+  
+    switch(day){
+        case 1 : printf("no\n");
+            break;
+        case 2 : printf("no\n");
+            break;
+        case 3 : printf("no\n");
+            break;
+        case 4 : printf("no\n");
+            break;
+        case 5 : printf("no\n");
+            break;
+        case 6 : printf("yes\n");
+            break;
+        case 7 : printf("yes");
+            break;
+                
+    }
+  
+    return 0;  
+}
