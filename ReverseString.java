bbb065dc33a81b2c19f7ce4c1e9df4f2455b1927
aishaansari29 <44176334+aishaansari29@users.noreply.github.com>
2019-03-09 11:39:49 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+import java.io.*; 
+import java.util.*; 
+  
+// Class of ReverseString 
+class ReverseString 
+{ 
+    public static void main(String[] args) 
+    { 
+        Scanner sc=new Scanner(System.in);
+        
+        String input = sc.nextLine();
+  
+        // convert String to character array 
+        // by using toCharArray 
+        char[] try1 = input.toCharArray(); 
+  
+        for (int i = try1.length-1; i>=0; i--) 
+            System.out.print(try1[i]); 
+    } 
+} 
